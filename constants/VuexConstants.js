2036d6596e284b19156e7dfc9b5ff6eb3c4507f1
Julian Salomon <jesalomont@unal.edu.co>
2019-10-26 19:12:20 -0500

    + Store for Events

--- constants/VuexConstants.js
+++ constants/VuexConstants.js
@@ -2,7 +2,14 @@ export const ACTIONS = {
   LOGIN: 'auth/login',
   GET_FILE: 'file/get',
   UPLOAD_FILE: 'file/upload',
+
   ANIMAL_LIST: 'animal/getList',
   ANIMAL_CREATE: 'animal/create',
   ANIMAL_GET: 'animal/get',
+
+  EVENT_LIST: 'event/getList',
+  EVENT_GET: 'event/get',
+  EVENT_SAVE: 'event/save',
+  EVENT_DELETE: 'event/delete',
+  EVENT_RESET: 'event/reset',
 }
---
+++ store/event.js
@@ -0,0 +1,78 @@
+import GraphQLUtil from '~/util/GraphQL'
+
+export const state = () => ({
+  list: [],
+  item: undefined,
+})
+
+export const mutations = {
+  setList(state, events) {
+    state.list = events
+  },
+  set(state, event) {
+    state.item = event
+  },
+}
+
+export const actions = {
+  async getList({ commit }) {
+    const fields = ['id', 'subject', 'description', 'date', 'animal_id']
+    const gql = {
+      type: 'query',
+      name: 'allEventsByUser',
+      params: [{ name: 'username', value: 'username', type: 'String!' }], // TODO: Change for session user
+      fields,
+    }
+    const data = await GraphQLUtil.request(this.$axios, gql)
+    if (data) {
+      commit('setList', data)
+    } else {
+      console.error('Not able to load events')
+    }
+  },
+  async get({ commit }, id) {
+    const fields = ['id', 'subject', 'description', 'date', 'animal_id']
+    const gql = {
+      type: 'query',
+      name: 'eventById',
+      params: [{ name: 'id', value: id, type: 'String!' }],
+      fields,
+    }
+    const event = await GraphQLUtil.request(this.$axios, gql)
+    if (event) {
+      commit('set', event)
+    } else {
+      console.error('Not able to load event')
+    }
+  },
+  reset({ commit }) {
+    commit('set', undefined)
+  },
+  async save({ commit }, { id, event }) {
+    const fields = ['id', 'subject', 'description', 'date', 'animal_id']
+    const gql = {
+      type: 'mutation',
+      name: 'createEvent',
+      params: [{ name: 'event', value: event, type: 'EventInput!' }],
+      fields,
+    }
+    if (id) {
+      const params = [...gql.params]
+      params.unshift({ name: 'id', value: id, type: 'String!' })
+      Object.assign(gql, { name: 'updateEvent', params })
+    }
+    const savedEvent = await GraphQLUtil.request(this.$axios, gql)
+    return Boolean(savedEvent)
+  },
+  async delete({ commit }, id) {
+    const fields = ['id', 'subject', 'description', 'date', 'animal_id']
+    const gql = {
+      type: 'mutation',
+      name: 'deleteEvent',
+      params: [{ name: 'id', value: id, type: 'String!' }],
+      fields,
+    }
+    const deletedEvent = await GraphQLUtil.request(this.$axios, gql)
+    return Boolean(deletedEvent)
+  },
+}
