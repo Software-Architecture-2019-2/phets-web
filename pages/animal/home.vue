5845476065cb977422304dab15d1e8c9fae5a2d8
Brandon Avilan <bsavilanr@slabcode.com>
2019-10-31 02:34:32 -0500

    Mis mascotas tabs added

--- pages/animal/home.vue
+++ pages/animal/home.vue
@@ -7,41 +7,93 @@
           <b-button to="/animal/create" variant="primary">Agregar Mascota</b-button>
         </b-row>
         <b-row v-if="own && own.length">
-          <b-col v-for="(animal, i) in own" :key="i" lg="4" md="6" sm="12" class="mb-4">
-            <b-card no-body class="animal-card">
-              <file-preview v-if="animal.media.length" :id="animal.media[0]" />
-              <b-card-img
-                v-else
-                :src="
+          <div>
+            <b-card no-body>
+              <b-tabs card>
+                <b-tab class="home-tab" title="Adopción" active>
+                  <b-col
+                    v-for="(animal, i) in own"
+                    :key="i"
+                    lg="4"
+                    md="6"
+                    sm="12"
+                    class="mb-4"
+                    :style="[ animal.adoption ? {'display':'visible'} : {'display':'none'}]"
+                  >
+                    <b-card v-if="animal.adoption" no-body class="animal-card">
+                      <file-preview v-if="animal.media.length" :id="animal.media[0]" />
+                      <b-card-img
+                        v-else
+                        :src="
                       `https://placekitten.com/480/210?image=${Math.floor(
                         Math.random() * 20
                       )}`
                     "
-                alt="Image"
-              />
-              <!-- TODO: Change for default image -->
-              <b-card-body>
-                <b-card-title>
-                  <b-link variant="link" @click="toPetProfile(animal.id)">
-                    {{
-                    animal.name
-                    }}
-                  </b-link>
-                  {{ age(animal.birthdate) }}
-                </b-card-title>
-                <b-card-sub-title>
-                  {{ animal.animal_type.value }}, {{ animal.breed }},
-                  {{ gender(animal.gender) }}
-                </b-card-sub-title>
-              </b-card-body>
+                        alt="Image"
+                      />
+                      <!-- TODO: Change for default image -->
+                      <b-card-body>
+                        <b-card-title>
+                          <b-link variant="link" @click="toPetProfile(animal.id)">
+                            {{
+                            animal.name
+                            }}
+                          </b-link>
+                          {{ age(animal.birthdate) }}
+                        </b-card-title>
+                        <b-card-sub-title>
+                          {{ animal.animal_type.value }}, {{ animal.breed }},
+                          {{ gender(animal.gender) }}
+                        </b-card-sub-title>
+                      </b-card-body>
+                    </b-card>
+                  </b-col>
+                </b-tab>
+                <b-tab class="home-tab" title="Phets">
+                  <b-col
+                    v-for="(animal, i) in own"
+                    :key="i"
+                    lg="4"
+                    md="6"
+                    sm="12"
+                    class="mb-4"
+                    :style="[ !animal.adoption ? {'display':'visible'} : {'display':'none'}]"
+                  >
+                    <b-card no-body class="animal-card">
+                      <file-preview v-if="animal.media.length" :id="animal.media[0]" />
+                      <b-card-img
+                        v-else
+                        :src="
+                      `https://placekitten.com/480/210?image=${Math.floor(
+                        Math.random() * 20
+                      )}`
+                    "
+                        alt="Image"
+                      />
+                      <!-- TODO: Change for default image -->
+                      <b-card-body>
+                        <b-card-title>
+                          <b-link variant="link" @click="toPetProfile(animal.id)">
+                            {{
+                            animal.name
+                            }}
+                          </b-link>
+                          {{ age(animal.birthdate) }}
+                        </b-card-title>
+                        <b-card-sub-title>
+                          {{ animal.animal_type.value }}, {{ animal.breed }},
+                          {{ gender(animal.gender) }}
+                        </b-card-sub-title>
+                      </b-card-body>
+                    </b-card>
+                  </b-col>
+                </b-tab>
+              </b-tabs>
             </b-card>
-          </b-col>
+          </div>
         </b-row>
         <b-row v-else class="home-else">
-          <b-col>
-            En el momento no tiene mascotas registradas / No se han encontrado
-            resultados para la búsqueda
-          </b-col>
+          <b-col>En el momento no tiene mascotas registradas</b-col>
         </b-row>
       </b-col>
     </b-row>
@@ -69,6 +121,7 @@ export default {
   created() {
     this.$store.dispatch(ACTIONS.USER_GET_PROFILE)
     this.$store.dispatch(ACTIONS.ANIMAL_OWN)
+    console.log(this.own)
   },
   methods: {
     age(birthdate) {
@@ -118,6 +171,11 @@ export default {
 
 .home-else {
   margin-top: 25%;
+}
+
+.home-tab {
+  display: flex;
+  flex-wrap: wrap;
 }
 
 .title {
